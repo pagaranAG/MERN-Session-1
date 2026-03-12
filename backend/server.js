76259bc8cb1e3a75bccd3bd60ef2302488d8065c
Adrienne Pagaran <pagaranag@students.nu-laguna.edu.ph>
2026-03-12 08:16:52 +0800

    current

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
  
 const express = require('express');
+const mongoose = require('mongoose');
+
+
+
 const notesRouter = require('./routes/notes'); 
 const app = express();
 
@@ -11,9 +15,13 @@ app.use((req, res, next) => {
 
 app.use('/api/notes', notesRouter); 
 
- 
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log('Server and Database are running on port', process.env.PORT);  
+    })
+}).catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+}); 
 
-app.listen(process.env.PORT, () => {
-        console.log('Server is running on port', process.env.PORT);
-    });
+
 
