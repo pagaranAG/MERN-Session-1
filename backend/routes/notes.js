0d1d3f6e53f1ee9c89e85f4098f217ea2eba80fa
Adrienne Pagaran <pagaranag@students.nu-laguna.edu.ph>
2026-03-12 08:58:17 +0800

    act1

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,16 +1,27 @@
 const express = require('express');
-
+const Note = require('../models/nodeModel');
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-   // res.send('Hello World! subasaskrayb 2 my yutube chanil');
+router.get('/', (req, res) => {  
     res.json({ 
         message: 'Hello World! subasaskrayb 2 my yutube chanil',
         name: 'Subas',
         age: 30,
         hobbies: ['coding', 'gaming', 'traveling']
     });
+});
+
+router.post('/',async (req,res) => {
+    const{title, content} = req.body
+    try{
+        const note = await Note.create ({title, content });
+        res.status(200).json(note);
+    }
+    catch(error){
+        res.status(400).json({error: error.message});
+    }
+    res.json({message: 'Note created successfully!'});
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const mongoose = require('mongoose');
 const notesRouter = require('./routes/notes'); 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
